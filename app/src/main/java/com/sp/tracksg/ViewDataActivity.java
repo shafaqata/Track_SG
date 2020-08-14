b541600b3bd08851e1484aee6d71defd71ac44fa
shafaqata <bscsworld@gmail.com>
2020-08-13 19:49:40 -0700

    Last

--- app/src/main/java/com/sp/tracksg/ViewDataActivity.java
+++ app/src/main/java/com/sp/tracksg/ViewDataActivity.java
@@ -4,68 +4,105 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewDataActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    ImageView mallImage;
-    TextView mallAddress;
+    String link;
+
+    WebView webView;
+    ProgressBar progressBarCicle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_data);
-        toolbar=findViewById(R.id.appbar);
+        toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        progressBarCicle=findViewById(R.id.progressBarCicle);
 
-        mallImage=findViewById(R.id.mallImage);
-        mallAddress=findViewById(R.id.mallAddress);
-
-
-        String name=getIntent().getStringExtra("key");
+        String name = getIntent().getStringExtra("key");
 
 
-
-        if (name.equals("CausewayPoint"))
-        {
+        if (name.equals("CausewayPoint")) {
             getSupportActionBar().setTitle("Causeway Point");
-            mallImage.setImageResource(R.drawable.causeway);
-            mallAddress.setText("1 Woodlands Square Singapore 738099");
+            link = "https://www.causewaypoint.com.sg";
         }
-        if (name.equals("Junction8"))
-        {
+      else if (name.equals("Junction8")) {
             getSupportActionBar().setTitle("Junction 8");
-            mallImage.setImageResource(R.drawable.juntion);
-            mallAddress.setText("9 Bishan Pl, Singapore 579837");
-
+            link = "https://www.capitaland.com/sg/malls/junction8/en.html";
         }
-        if (name.equals("NorthpointCity"))
-        {
+        else if (name.equals("NorthpointCity")) {
             getSupportActionBar().setTitle("Northpoint City");
-            mallImage.setImageResource(R.drawable.northpoint);
-            mallAddress.setText("930 Yishun Avenue 2 Singapore 769098");
+            link = "https://www.northpointcity.com.sg";
         }
-        if (name.equals("WaterwayPoint"))
-        {
+        else if (name.equals("WaterwayPoint")) {
             getSupportActionBar().setTitle("Waterway Point");
-            mallImage.setImageResource(R.drawable.waterway);
-            mallAddress.setText("83 Punggol Central Singapore 828761");
+            link = "https://www.waterwaypoint.com.sg/";
         }
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl(link);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(
+                new SSLTolerentWebViewClient()
+        );
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                if (newProgress > 80) {
+                    progressBarCicle.setVisibility(View.GONE);
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
 
 
     }
+    private class SSLTolerentWebViewClient extends WebViewClient {
 
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed(); // Ignore SSL certificate errors
+        }
 
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
